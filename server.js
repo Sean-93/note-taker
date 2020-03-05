c0414d922d9c078289f176f21c67f4774d2ac1a9
Sean Rooney <seandanielrooney@gmail.com>
2020-03-05 11:20:09 -0700

    edited server.js

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const path= require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/' + '/public/index.html', function(err){
-        console.log(err);
-    });
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+  
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+  });
+
+// app.get('/', function(req, res){
+//     res.sendFile(__dirname + '/' + '/public/index.html', function(err){
+//         console.log(err);
+//     });
     
-});
+// });
 app.listen(8080, function(){
     console.log('app successful!')
     console.log("DIR: ", __dirname);
